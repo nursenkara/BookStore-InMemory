fb43e882d38930e9b6ea997bad12cbad830c569c
Nur Şenkara <nsenkara19@gmail.com>
2022-04-14 14:40:45 +0300

    Book Store CRUD işlemler EntityFramework.InMemory ile yapıldı.

---
+++ BookStore/Controllers/BooksController.cs
@@ -0,0 +1,73 @@
+﻿using BookStore.DBOperations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+        private readonly BookStoreDbContext _context;
+        public BooksController(BookStoreDbContext context)
+        {
+            _context = context; 
+        }
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            var bookList = _context.Books.OrderBy(x => x.Id).ToList<Book>();
+            return bookList;
+        }
+        [HttpGet("{id}")]
+        public Book GetById(int id)
+        {
+            var book = _context.Books.Where(x => x.Id == id).SingleOrDefault();
+            return book;
+        }
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book newBook)
+        {
+            var book =  _context.Books.SingleOrDefault(x => x.Title == newBook.Title);
+            if(book is not null)
+            {
+                return BadRequest();
+            }
+            _context.Books.Add(newBook);
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
+        {
+            var book = _context.Books.SingleOrDefault(x => x.Id == id);
+            if(book is null)
+            {
+                return BadRequest();
+            }
+            book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : book.GenreId;
+            book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+            book.PublishedDate = updatedBook.PublishedDate != default ? updatedBook.PublishedDate :book.PublishedDate;
+            book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+            _context.SaveChanges();
+            return Ok();
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = _context.Books.SingleOrDefault(x => x.Id==id);
+            if(book is null)
+            {
+                return BadRequest();
+            }
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ BookStore/DBOperations/DataGenerator.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace BookStore.DBOperations
+{
+    public class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BookStoreDbContext(serviceProvider.GetRequiredService<DbContextOptions<BookStoreDbContext>>()))
+            {
+                if (context.Books.Any())
+                {
+                    return;
+                }
+
+                context.Books.AddRange(
+                new Book
+                {
+                    Id = 1,
+                    Title = "Me Before You",
+                    GenreId = 1,//Love
+                    PageCount = 500,
+                    PublishedDate = new DateTime(2014, 03, 04)
+                },new Book
+                {
+                    Id = 2,
+                    Title = "Harry Potter",
+                    GenreId = 2,//Adventure
+                    PageCount = 250,
+                    PublishedDate = new DateTime(2000, 01, 05)
+                },new Book
+                {
+                    Id = 3,
+                    Title = "Lord of The Rings",
+                    GenreId = 2,//Adventure
+                    PageCount = 1200,
+                    PublishedDate = new DateTime(2003, 07, 25)
+                }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
